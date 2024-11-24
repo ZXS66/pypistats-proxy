5fb3752a2f12a6cfbd7534b5bfaf5d39c2e9f198
ZXS66 <zxs.dev@outlook.com>
2024-11-24 10:37:54 +0800

    first GA version

---
+++ Program.cs
@@ -0,0 +1,136 @@
+using Microsoft.Extensions.Caching.Memory;
+using MemoryCache cache = new(new MemoryCacheOptions());
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("https://pypi.org")
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+#if !DEBUG
+app.UseHttpsRedirection();
+#endif
+
+app.MapPost("/package/{package_id}", GetDownloadStats).WithName(nameof(GetDownloadStats)).WithOpenApi();
+app.UseCors();
+
+app.Run();
+
+#region main logic
+
+// ISet<String> _ALLOW_HOSTS = new HashSet<string>() { "pypi.org" };
+
+async Task<DownloadStats?> GetDownloadStats(string package_id, [Microsoft.AspNetCore.Mvc.FromHeader(Name = "referer")] string referrerHeader = "")
+{
+    if (string.IsNullOrWhiteSpace(package_id))
+    {
+        return null;
+    }
+#if !DEBUG
+    if (referrerHeader != $"https://pypi.org/project/{package_id}")
+    {
+        return null;
+    }
+#endif
+    if (cache.TryGetValue(package_id, out DownloadStats? cachedValue))
+    {
+        return cachedValue;
+    }
+    string html = await GetHTMLFromUrl($"https://pypistats.org/packages/{package_id.Trim()}");
+    var ret = ParseHtml(html, package_id);
+    if (ret == null)
+    {
+        return null;
+    }
+    cache.Set(package_id, ret, TimeSpan.FromHours(8));
+    return ret;
+};
+
+async Task<string> GetHTMLFromUrl(string url)
+{
+    int maxRetries = 3;
+    for (int i = 0; i < maxRetries; i++)
+    {
+        try
+        {
+            using var client = new HttpClient();
+            return await client.GetStringAsync(url);
+        }
+        catch (Exception)
+        {
+            await Task.Delay(1024);
+        }
+    }
+    return String.Empty;
+}
+
+DownloadStats? ParseHtml(string html, string package_id)
+{
+    if (string.IsNullOrWhiteSpace(html))
+    {
+        return null;
+    }
+    int downloads_flag = 0;
+    int downloads_lastday = 0, downloads_lastweek = 0, downloads_lastmonth = 0;
+    foreach (string line in html.Split('\n'))
+    {
+        if (line == "Downloads last day:")
+        {
+            downloads_flag = 1;
+            continue;
+        }
+        else if (line == "Downloads last week:")
+        {
+            downloads_flag = 2;
+            continue;
+        }
+        else if (line == "Downloads last month:")
+        {
+            downloads_flag = 3;
+            continue;
+        }
+        if (downloads_flag > 0 && int.TryParse(line.Replace(",", String.Empty), out int downloads))
+        {
+            switch (downloads_flag)
+            {
+                case 1: downloads_lastday = downloads; break;
+                case 2: downloads_lastweek = downloads; break;
+                case 3: downloads_lastmonth = downloads; break;
+            }
+            if (downloads_flag == 3)
+            {
+                break;
+            }
+        }
+    }
+    if (downloads_flag == 0)
+    {
+        // can not find any downloads amount from the page
+        return null;
+    }
+    return new DownloadStats(package_id, downloads_lastday, downloads_lastweek, downloads_lastmonth);
+}
+
+record DownloadStats(string package_id, int downloads_lastday, int downloads_lastweek, int downloads_lastmonth);
+
+#endregion
---
+++ README.md
@@ -0,0 +1,3 @@
+This repo just grab data from [pypistats.org](https://pypistats.org), that enable you to find downloads amount of given python packages, which query Google Cloud BigTable of pypi.org backend database.
+
+For common usages, please browse [the site](https://pypistats.org) directly.

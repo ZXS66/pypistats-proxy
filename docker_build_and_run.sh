5d6b8411dd5b5b482d96c9087beffc47817c7b17
ZXS66 <zxs.dev@outlook.com>
2024-11-24 16:59:32 +0800

    stop and remove existing docker container before build and run it

--- docker_build_and_run.sh
+++ docker_build_and_run.sh
@@ -1,3 +1,6 @@
 docker build -t pypistats-proxy:0.0.1 .
+# stop and remove existing pod
+docker stop pypistats-proxy-pod
+docker rm pypistats-proxy-pod
 docker run -itd --restart=always -p 8080:8080 --name pypistats-proxy-pod pypistats-proxy:0.0.1
 docker ps
